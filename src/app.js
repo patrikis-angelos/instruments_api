671fe81b568bf2e1ba6d52c1168318b3c0ad95ce
apatrikis <apatrikis@pccwglobal.com>
2022-12-03 19:37:27 +0200

    Ignore top level await eslint

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import dbConnect from './startup/db.js';
 
 const app = express();
+
+// TODO: Ignore rule in eslintrc
+//eslint-disable-next-line
 await dbConnect();
 
 app.use('/', (req, res) => {
