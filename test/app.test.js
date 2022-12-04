3496458e0207d4aadeb50901bd01fa7bb88131dc
apatrikis <apatrikis@pccwglobal.com>
2022-12-04 16:24:56 +0200

    Fix tests

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,6 @@ import app from '../src/app.js';
 describe('Hello Test', () => {
   test('GET /', async () => {
     const response = await request(app).get('/');
-    expect(response.status).toBe(400);
+    expect(response.status).toBe(200);
   });
 });
