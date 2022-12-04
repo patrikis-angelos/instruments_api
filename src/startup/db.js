70d2199b90333cc4959e9b55be0800154e159695
apatrikis <apatrikis@pccwglobal.com>
2022-12-04 20:19:59 +0200

    Create category table

--- src/startup/db.js
+++ src/startup/db.js
@@ -1,16 +1,20 @@
 import { DataSource } from 'typeorm';
 import config from 'config';
+import migrations from '../migrations/index.js';
+import entities from '../models/index.js';
 
-const myDataSource = new DataSource({
+export const myDataSource = new DataSource({
   type: config.get('db.type'),
   host: config.get('db.host'),
   port: config.get('db.port'),
   username: config.get('db.user'),
   password: config.get('db.password'),
   database: config.get('db.name'),
-  entities: ['src/models/*.js'],
+  entities: entities,
+  migrations: migrations,
+  migrationsTableName: 'migrations',
   logging: config.get('db.logging'),
-  synchronize: true,
+  synchronize: false,
   extra: {
     idleTimeoutMillis: config.get('db.idleTimeoutMillis'),
   }
