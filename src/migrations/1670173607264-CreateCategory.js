78c0b64cc3e7457bba427389da8c99b81e8afde7
apatrikis <apatrikis@pccwglobal.com>
2023-01-14 15:34:26 +0200

    Change id of category to uuid

--- src/migrations/1670173607264-CreateCategory.js
+++ src/migrations/1670173607264-CreateCategory.js
@@ -2,7 +2,8 @@ export default class CreateCategory1670173607264 {
   async up(queryRunner) {
     await queryRunner.query(
       `CREATE TABLE "category" (
-        id SERIAL PRIMARY KEY,
+        id uuid DEFAULT uuid_generate_v4 (),
+        PRIMARY KEY (id),
         name VARCHAR( 50 ) NOT NULL,
         created_at TIMESTAMP NOT NULL DEFAULT NOW(),
         updated_at TIMESTAMP
