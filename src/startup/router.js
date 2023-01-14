53b62f9b9b54381fe836fb20c31faaeae848b673
apatrikis <apatrikis@pccwglobal.com>
2023-01-14 17:04:15 +0200

    Create error middleware

--- src/startup/router.js
+++ src/startup/router.js
@@ -4,5 +4,6 @@ import homeRouter from '../routes/home.js';
 const router = express.Router();
 
 router.use('/', homeRouter);
+router.use((req, res) => { res.status(404).json({ message: 'Not Found' }); });
 
 export default router;
